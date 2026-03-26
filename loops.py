29f3a001305899e5afef4114525f67504aabb381
hasibprangon <hasibulhossainp09@gmail.com>
2026-03-26 17:02:32 +0600

    for loops in python

---
+++ loops.py
@@ -0,0 +1,13 @@
+##loops in python
+
+# for loop
+for i in range(1,10,1):
+    # print(i, "Hello world")
+    break
+
+
+# nested for loop
+
+for i in range(1, 6):
+    for j in range(1, 5):
+        print(i, j, "*")
